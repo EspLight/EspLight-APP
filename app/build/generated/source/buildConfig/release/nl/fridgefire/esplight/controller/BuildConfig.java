a13b2b1c28e8a19bef525ef496e3865f235637f7
Michiel Brink <github@pb4m.nl>
2015-12-12 20:44:12 +0100

    add build directory

---
+++ app/build/generated/source/buildConfig/release/nl/fridgefire/esplight/controller/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nl.fridgefire.esplight.controller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "nl.fridgefire.esplight.controller";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "2.3";
+}
